0bd248a3553e47a05612462db9cd7044ba663eb3
leshawn-rice <leshawnrice3@gmail.com>
2021-09-19 20:32:23 -0700

    changed name of extra vars name -> project_name

--- run.sh
+++ run.sh
@@ -22,4 +22,4 @@ source="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 
 playbook="$source/playbook.yml"
 
-ansible-playbook $playbook -e "name=$project author=$author path=$path description=$description secret_key=$secret database=$database default_port=$port source_dir=$source"
+ansible-playbook $playbook -e "project_name=$project author=$author path=$path description=$description secret_key=$secret database=$database default_port=$port source_dir=$source"
